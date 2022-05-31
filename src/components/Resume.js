e33642038089fd0a005416e5f148b3cbfb754252
David Griffin <95205646+DavidTJGriffin@users.noreply.github.com>
2022-05-31 13:26:20 -0700

    Updated file path for resume pdf

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Resume() {
   return (
       <div id='resume-section'>
-    <p className='res-link'>Download a copy of my resume <a className='resume-link' href={process.env.PUBLIC_URL + '/images/david-resume.pdf'}>here</a>.</p>
+    <p className='res-link'>Download a copy of my resume <a className='resume-link' href={process.env.PUBLIC_URL + '/assets/images/david-resume.pdf'}>here</a>.</p>
     </div>
   )
 }
