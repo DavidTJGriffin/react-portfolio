a15d47262f305546944643e21fd5f7ecc4c35121
David Griffin <david.dtjg2001@gmail.com>
2022-05-31 18:36:03 -0700

    fixed hamburger menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Navigation(props) {
     const { currentPage, setCurrentPage } = props;
@@ -6,55 +6,64 @@ function Navigation(props) {
     useEffect(() => {
         document.title = currentPage
       }, [currentPage]);
-
       const hamburger = document.querySelector(".hamburger");
       const navMenu = document.querySelector(".nav-menu");
-
-
-      function mobileMenu() {
-          hamburger.classList.toggle("active");
-          navMenu.classList.toggle("active");
-      }
       const navLink = document.querySelectorAll(".nav-link");
+      const [isActive, setActive] = useState("true");
 
-      navLink.forEach(n => n.addEventListener("click", closeMenu));
+      const handleToggle = () => {
+        setActive(!isActive);
+      };
 
-      function closeMenu() {
-          hamburger.classList.remove("active");
-          navMenu.classList.remove("active");
-      }
+    //   function mobileMenu() {
+    //     hamburger.classList.toggle("active");
+    //     navMenu.classList.toggle("active");
+    // }
+    
+
+    // function closeMenu() {
+    //     hamburger.classList.remove("active");
+    //     navMenu.classList.remove("active");
+    // }
+      
   return (
+      
     <nav className="navbar">
             <a href="#" className="nav-logo">David</a>
-            <ul className="nav-menu">
-                <li className="nav-item">
+            <ul className={`nav-menu ${isActive ?  null : "active"}`}>
+                <li className="nav-item" onClick={handleToggle}>
                     <div className="nav-link">
                             <a onClick={() => setCurrentPage('About')}>About</a>
                         </div>
                 </li>
-                <li className="nav-item">
-                    <div className="nav-link">
+                <li className="nav-item" onClick={handleToggle}>
+                    <div className={`nav-link ${isActive ? null : "active"}`}>
                        <a onClick={() => setCurrentPage('Work')}>Work</a>
                         </div>
                 </li>
-                <li className="nav-item">
-                    <div className="nav-link">
+                <li className="nav-item" onClick={handleToggle}>
+                    <div className={`nav-link ${isActive ? null : "active"}`}>
                     <a onClick={() => setCurrentPage('Contact')}>Contact</a>
                         
                         </div>
                 </li>
-                <li className="nav-item">
-                <a className="nav-link">
+                <li className="nav-item" onClick={handleToggle}>
+                <div className={`nav-link ${isActive ? null : "active"}`}>
                 <a onClick={() => setCurrentPage('Resume')}>Resume</a>
-                </a>
+                </div>
                 </li>
             </ul>
-            <div className="hamburger" onClick={mobileMenu}>
+            <div className={`hamburger ${isActive ?  null : "active"}`} onClick={handleToggle}>
                 <span className="bar"></span>
                 <span className="bar"></span>
                 <span className="bar"></span>
             </div>
         </nav>
   )
+
+  
+
+
+     
 }
 export default Navigation
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Resume() {
   return (
       <div id='resume-section'>
+           <h1 className="title-name">RESUME</h1>
     <p className='res-link'>Download a copy of my resume <a className='resume-link' href={process.env.PUBLIC_URL + '/assets/images/david-resume.pdf'}>here</a>.</p>
     </div>
   )
