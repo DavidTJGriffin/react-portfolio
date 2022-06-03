75045ce2068f7ef2e78150019d384c51acacf0ff
David Griffin <david.dtjg2001@gmail.com>
2022-06-02 23:59:39 -0700

    updated resume component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import resumeScreenshot from "../assets/images/resumeScreenshot.png"
 
 function Resume() {
   return (
-      <div id='resume-section'>
-           <h1 className="title-name">RESUME</h1>
-    <p className='res-link'>Download a copy of my resume <a className='resume-link' href={process.env.PUBLIC_URL + '/assets/images/david-resume.pdf'}>here</a>.</p>
+    <div id='resume-section'>
+      <h1 className="title-name" id='resume-title'>RESUME</h1>
+      <p className='res-link'>Download a copy of my resume by clicking the image below: </p>
+        <div className="project1" style={{ backgroundImage: `url(${resumeScreenshot})` }}>
+                <div className="project-name">
+                    <h3>Resume</h3>
+                </div>
+                <a href={process.env.PUBLIC_URL + '/assets/images/david-resume.pdf'}></a>
+            </div>
+
+       
     </div>
   )
 }
